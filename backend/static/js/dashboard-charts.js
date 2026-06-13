04b65a850d66000664a19c25eb940645773c4cb4
Tu Nombre <aronbarrientos98@gmail.com>
2026-06-12 20:37:30 -0400

    cambios en css

--- backend/static/js/dashboard-charts.js
+++ backend/static/js/dashboard-charts.js
@@ -26,8 +26,15 @@
   ];
 
   function colors(n) {
+    var arr = palette.slice();
+    for (var i = arr.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
     var out = [];
-    for (var i = 0; i < n; i++) out.push(palette[i % palette.length]);
+    for (var i = 0; i < n; i++) out.push(arr[i % arr.length]);
     return out;
   }
 
